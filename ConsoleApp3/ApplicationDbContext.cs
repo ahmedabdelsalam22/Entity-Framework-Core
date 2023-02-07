826c48228510a9c875699e99dd743ac6594821f1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-07 09:45:55 +0200

    Add project files.

---
+++ ConsoleApp3/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-IG5PLK5;Initial Catalog=BikeStore;Integrated Security=True");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // obtain dataSource 
+            var context = new ApplicationDbContext();
+
+            Employee employee = new Employee 
+            {
+             Name= "Ahmed",
+             City = "Cairo",
+             Address = "nasr city"
+            };
+
+            context.employees.Add(employee);
+            context.SaveChanges();
+        }
+    }
+}
