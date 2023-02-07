dcdd4924483ae279041cca15a99468ddf156d797
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-07 10:32:48 +0200

    add Models folder

--- ConsoleApp3/Models/Employee.cs
+++ ConsoleApp3/Models/Employee.cs
@@ -5,11 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EFCore
+namespace EFCore.Models
 {
     public class Employee
     {
-        public int Id { get; set; }      
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
