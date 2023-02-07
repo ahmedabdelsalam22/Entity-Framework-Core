3c3dce00dff10a722c2badf064e54193ac984bdd
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-07 10:29:53 +0200

    Api Fluent (make db column required) => OnModelCreating

--- ConsoleApp3/ApplicationDbContext.cs
+++ ConsoleApp3/ApplicationDbContext.cs
@@ -10,6 +10,14 @@ namespace EFCore
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Employee> employees { get; set; }
+
+        // Api Fluent (make db column required) => OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Property(p=>p.Name)
+                .IsRequired();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-IG5PLK5;Initial Catalog=BikeStore;Integrated Security=True");
---
+++ ConsoleApp3/Migrations/20230207080148_PopulateDate.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Migrations
+{
+    public partial class PopulateDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO employees VALUES ('Omer','Alexandria','Alwx15-St')"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DELETE FROM employees WHERE name = 'Omer' "
+                );
+        }
+    }
+}
